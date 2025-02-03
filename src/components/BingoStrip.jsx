e9444a30cc1b57a4ba13c6dd2155a12456f6922d
zainaejaz <zainaijaz32@gmail.com>
2025-02-03 22:01:54 +0500

    updated  commit

--- src/components/BingoStrip.jsx
+++ src/components/BingoStrip.jsx
@@ -1,3 +1,4 @@
+//dummy values for bingo game now...
 const stripData = [
   [
     1, 0, 0, 0, 41, 0, 63, 70, 81, 0, 15, 29, 31, 47, 51, 0, 0, 0, 3, 19, 0, 34,
@@ -24,26 +25,25 @@ const stripData = [
     0, 0, 65, 79, 0,
   ],
 ];
-
+// BingoStrip Component
 const BingoStrip = () => {
   return (
     <div className="flex flex-col items-center w-full p-4 bg-[#dc8cb0]">
       {stripData.map((card, index) => (
         <div key={index} className="flex items-center justify-center">
           {/* BINGO Text - Rotated Vertically */}
-          <div className="flex items-center justify-center w-[55px] sm:w-[66px] md:w-[68px] lg:w-[72px] h-full text-[30px] sm:text-[35px] md:text-[40px] lg:text-[45px] font-medium text-[black] transform -rotate-90 leading-none -mr-3">
+          <div className="mb-[14px] flex items-center text-[30px] sm:text-[40px] md:text-[42px] lg:text-[45px] font-medium text-[black] leading-none transform -rotate-90 -mr-7 sm:-mr-9">
             BINGO
           </div>
-
           {/* Bingo Table */}
-          <table className="bg-[#fae8e0] border-4 border-[black] shadow-md mb-4 w-full max-w-lg">
+          <table className="bg-[#fae8e0]  border-3 sm:border-5 h-[50%] sm:h-[70%] md:h-[80%] lg:h-full border-[black] shadow-md mb-4 w-full max-w-lg">
             <tbody>
               {[0, 1, 2].map((row) => (
                 <tr key={row}>
                   {card.slice(row * 9, row * 9 + 9).map((num, i) => (
                     <td
                       key={i}
-                      className={`w-12 h-12 border-4 border-[black] text-[10px] sm:text-[14px] md:text-[16px] lg:text-[18px] font-bold text-center`}
+                      className={`w-8 h-8 sm:w-12 sm:h-12  border-3 sm:border-5 border-[black] text-[10px] sm:text-[14px] md:text-[16px] lg:text-[18px] font-bold text-center`}
                     >
                       {num !== 0 ? num : ""}
                     </td>
