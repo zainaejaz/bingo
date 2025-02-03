5c6c6f93a8f4d4783d68ccd3014c1bb208213a64
zainaejaz <zainaijaz32@gmail.com>
2025-02-03 19:41:57 +0500

    updated  commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,8 @@
-// import { BrowserRouter, Route, Routes } from "react-router-dom";
-// import Users from "./components/Users";
-
 import BingoStrip from "./components/BingoStrip";
 
 function App() {
   return (
     <>
-      {/* <BrowserRouter>
-        <Routes>
-          <Route exact path="/" element={<Users />} />
-        </Routes>
-      </BrowserRouter> */}
       <BingoStrip />
     </>
   );
